c4313f7ad9cb368b0a7f5921745497014ffc8836
ctrouilh <ctrouilh@cisco.com>
2016-06-01 11:31:47 +0200

    ZeusClient on Go

--- main.go
+++ main.go
@@ -2,7 +2,10 @@ package main
 
 import (
     "fmt"
+	"github.com/CiscoZeus/go-zeusclient"
     "net/http"
+	"os"
+	"runtime"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
@@ -10,6 +13,32 @@ func handler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-    http.HandleFunc("/", handler)
+	// print env variables
+	fmt.Println("OS:", runtime.GOOS)
+	fmt.Println("USER:", os.Getenv("USER"))
+	fmt.Println("HOME:", os.Getenv("HOME"))
+	fmt.Println("HOSTNAME:", os.Getenv("HOSTNAME"))
+	z := &zeus.Zeus{ApiServ: "http://api.ciscozeus.io", Token: os.Getenv("ZEUS_TOKEN")}
+	logs := zeus.LogList{
+		Name: "syslog",
+		Logs: []zeus.Log{
+			zeus.Log{
+				"OS":       runtime.GOOS,
+				"USER":     os.Getenv("USER"),
+				"HOME":     os.Getenv("HOME"),
+				"HOSTNAME": os.Getenv("HOSTNAME")},
+		},
+	}
+	suc, err := z.PostLogs(logs)
+	if err != nil {
+		fmt.Println("Zeus response:", err.Error())
+	} else {
+		fmt.Println("Zeus response:", suc)
+	}
+
+ 	fmt.Printf("Starting http server...\n")
+ 	http.HandleFunc("/", handler)
     http.ListenAndServe(":8080", nil)
 }
+
+ 
